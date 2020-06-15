6d5a1faf9515d359e77f81a1c1019d192524faf8
Medhat Alkadri <medhatalkadri@gmail.com>
2020-06-15 20:53:38 +0200

    change app.js ajax connection to port 5050

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'http://localhost:3000/resumeData.json',
+      url:'http://localhost:5050/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
