aad603620f7ee2272365ceff9839dad966c14a8e
Medhat Alkadri <medhatalkadri@gmail.com>
2020-07-29 14:33:51 +0200

    add Certified Courses row, changed order of resume rows and added new course to resumeData.json

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -15,6 +15,18 @@ class Resume extends Component {
         </div>
       });
 
+      var courses = this.props.data.courses.map(function(course){
+        return <div key={course.school} className="row item">
+           <div className="twelve columns">
+              <h3>{course.school}</h3>
+              <p className="info">{course.degree}  <em className="date">{course.graduated}</em></p>
+              <ul style={{ paddingLeft: 50}}>
+              {course.description.map(d => <li key={d} style={{ listStyleType: "circle" }}>{d}</li>)}
+              </ul>
+           </div>
+        </div>
+      });
+
       var work = this.props.data.work.map(function(job){
         // {console.log(job.description[0])}
         return <div key={job.years} className="row item">
@@ -39,6 +51,16 @@ class Resume extends Component {
     }
     return (
       <section id="resume">
+      <div className="row work">
+         <div className="three columns header-col">
+            <h1><span>Work History</span></h1>
+         </div>
+
+         <div className="nine columns main-col">
+          {work}
+         </div>
+      </div>
+
       <div className="row education">
          <div className="three columns header-col">
             <h1><span>Education</span></h1>
@@ -49,13 +71,13 @@ class Resume extends Component {
          </div>
       </div>
 
-      <div className="row work">
+      <div className="row courses">
          <div className="three columns header-col">
-            <h1><span>Work</span></h1>
+            <h1><span>Certified Courses</span></h1>
          </div>
 
          <div className="nine columns main-col">
-          {work}
+            {courses}
          </div>
       </div>
 
